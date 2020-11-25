444b36284660660f617a4b569f5f6b16f86233ac
smarttin <smarttin77@gmail.com>
2020-11-25 01:59:32 +0100

    adds server endpoint

--- client/next.config.js
+++ client/next.config.js
@@ -8,6 +8,6 @@ module.exports = {
     return config;
   },
   env: {
-    BASE_URL: dev ? 'http://localhost:4000' : 'https://onyeka-g.herokuapp.com',
+    BASE_URL: dev ? 'http://localhost:4000' : 'https://onyeka-g.herokuapp.com/',
   },
 };
